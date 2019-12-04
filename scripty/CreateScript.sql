3fc6cf17e467a4368f0046c5b40abb671688f536
HayCZ <david.senohrabek@seznam.cz>
2019-12-04 14:18:05 +0100

    foreign key's obor_predmet

--- scripty/CreateScript.sql
+++ scripty/CreateScript.sql
@@ -24,11 +24,11 @@ CREATE TABLE obor_predmet
 ALTER TABLE obor_predmet
     ADD CONSTRAINT obor_predmet_pk PRIMARY KEY (studijni_obor_id_obor,
                                                 predmet_id_predmet);
-alter table OBOR_PREDMET
+alter table obor_predmet
     add constraint OBOR_PREDMET_PREDMETY_ID_PREDMET_fk
         foreign key (STUDIJNI_OBOR_ID_OBOR) references PREDMETY;
 
-alter table OBOR_PREDMET
+alter table obor_predmet
     add constraint OBOR_PREDMET_STUDIJNI_OBORY_ID_OBOR_fk
         foreign key (STUDIJNI_OBOR_ID_OBOR) references STUDIJNI_OBORY;
 
@@ -46,8 +46,7 @@ CREATE TABLE skupiny
 (
     id_skupina INTEGER      NOT NULL,
     nazev      VARCHAR2(50) NOT NULL,
-    popis      VARCHAR2(250),
-    id_predmet INTEGER      NOT NULL
+    popis      VARCHAR2(250)
 );
 
 ALTER TABLE skupiny
