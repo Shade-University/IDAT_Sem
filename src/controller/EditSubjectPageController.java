6b6004167342e188c0626d5da09c8cdecef86493
HayCZ <david.senohrabek@seznam.cz>
2019-12-12 17:00:27 +0100

    init.

--- src/controller/EditSubjectPageController.java
+++ src/controller/EditSubjectPageController.java
@@ -9,11 +9,29 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import model.Group;
+import model.Subject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EditSubjectPageController implements Initializable {
+
+
+    private static AdministrationPageController parent;
+
+    private static Subject editedSubject;
+
+    /**
+     * Nastavení vstupních parametrů
+     *
+     * @param subject - editovaná skupina
+     * @param aP    - Předek ve kterém je zobrazený formulář
+     */
+    public static void setParams(Subject subject, AdministrationPageController aP) {
+        editedSubject = subject;
+        parent = aP;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
